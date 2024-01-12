2c5382f262deb411bb222123fc3c9e3d67a64976
Aftab Ansari <aahftab@gmail.com>
2024-01-13 04:42:45 +0530

    minor changes

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.render("scoreboard.ejs", { title: "Scoreboard", users: users });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
