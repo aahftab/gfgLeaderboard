e1a6d6841334a0d57343ee0afb2354c0ac455155
Aftab Ansari <aahftab@gmail.com>
2024-01-15 01:30:30 +0530

    view directory corrected

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.static("./"));
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'))
 
 const users = { "themihirmathur": { "#school": { "count": 1 }, "#basic": { "count": 5 }, "#easy": { "count": 83 }, "#medium": { "count": 95 }, "#hard": { "count": 19 } }, "17amolgode": { "#school": { "count": 5 }, "#basic": { "count": 121 }, "#easy": { "count": 216 }, "#medium": { "count": 89 }, "#hard": { "count": 11 } }, "pk6176tpy": { "#school": { "count": 2 }, "#basic": { "count": 4 }, "#easy": { "count": 4 }, "#medium": { "count": 11 }, "#hard": { "count": 1 } }, "anuragfulare": { "#school": { "count": 6 }, "#basic": { "count": 18 }, "#easy": { "count": 35 }, "#medium": { "count": 3 }, "#hard": { "count": 0 } } };
 
